d7aa6943cf3564bf4059dc67b681732140b3bcf8
rex_xlw <rexmmxlw@gmail.com>
2016-01-02 16:14:37 -0500

    Using MongoDB 160102

--- leadScrapy/database.py
+++ leadScrapy/database.py
@@ -1,3 +1,7 @@
+
+import os
+"""
+#redis Verison
 import redis
 
 RS = redis.Redis(host='localhost', port=6379, db=1)
@@ -10,5 +14,28 @@ def isUrlExist(url):
 
 def insertIntoDB(url):
 	RS.set(url, 1)
+"""
 
+#MongoDB Version
+from pymongo import MongoClient
+HOST = os.environ['OPENSHIFT_MONGODB_DB_HOST']
+PORT = os.environ['OPENSHIFT_MONGODB_DB_PORT']
+connection = HOST + ":" + PORT
+conn = MongoClient(connection)
+#conn = MongoClient()
+leadScrapyDB = conn.leadScrapy
+urlCol = leadScrapyDB.urlCol
 
+def isUrlExist(url):
+	isExist = False
+	ele = {"url":url}
+	for flag in urlCol.find(ele):
+		isExist = True
+	if isExist ==  True:
+		print url
+		pause = raw_input("PAUSE")
+	return isExist
+
+def insertIntoDB(url):
+	ele = {"url":url}
+	urlCol.insert(ele)
